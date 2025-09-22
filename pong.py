56b942ddd1dcca639fdefa8708ddb86c0d8bc318
Jakucs Viktor <jakucs42@gmail.com>
2025-09-22 22:51:28 +0200

    Ball added

--- pong.py
+++ pong.py
@@ -14,6 +14,11 @@ paddle_height = 10
 paddle_x = (WIDTH-paddle_width) // 2
 paddle_y = HEIGHT - 30
 paddle_speed = 6
+ball_x = 100
+ball_y = 100
+ball_speed_x = 4
+ball_speed_y = 4
+ball_radius = 10
 
 
 
@@ -22,7 +27,7 @@ while over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             over = False
-        elif event.type == pygame.KEYDOWN: # Next if, we check buttons.
+        elif event.type == pygame.KEYDOWN: # Next if, we will check buttons.
             if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
                 control_mode = "keyboard"
         elif event.type == pygame.MOUSEMOTION:
@@ -30,6 +35,7 @@ while over:
     print("Történés: ", event)
     screen.fill((0, 0, 0))  # background
     pygame.draw.rect(screen, (255, 255, 255), (paddle_x, paddle_y, paddle_width, paddle_height))
+    pygame.draw.circle(screen, (255, 255, 255), (ball_x, ball_y), ball_radius)
     pygame.display.flip()
     clock.tick(60) # We use clock object. Max 60 round / second
 
